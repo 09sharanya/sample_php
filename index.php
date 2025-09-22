1fc13ba26b4f709d0d91dd30b85b1e9aab2efc52
09sharanya <sharanya200309@gmail.com>
2025-09-22 16:36:19 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$appName = $_ENV['APP_NAME'] ?? 'App';
+$greeting = $_ENV['GREETING'] ?? 'Hi';
+
+echo "<h1>$greeting, welcome to $appName!</h1>";
