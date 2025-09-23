902a1f278aa6ba291571ee5d3d3f7e449511f1ea
09sharanya <sharanya200309@gmail.com>
2025-09-23 14:59:24 +0530

    test3

--- index.php
+++ index.php
@@ -14,3 +14,5 @@ echo "<h1>sharanya, welcome to $appName</h1>";
 echo "<h1>hey kavya , this is $appName</h1>";
 echo "<h1>$greeting , this is testinggg for $appName</h1>";
 echo "<h1>$greeting kavya ans sharanya , this is testinggg for $appName</h1>";
+echo "<h1>$greeting, welcome to $appName!</h1>";
+echo "<h1>sharanya, welcome to $appName</h1>";
